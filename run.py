dababbeb769a2bc6505f5aba6cbdb919fa12fdc6
Oliver Morse <Olimorse_90@Hotmail.co.uk>
2024-07-24 09:13:15 +0000

    Add main title scroll function into main function

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import time
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -16,8 +17,31 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Oathbound')
 
-enemies = SHEET.worksheet('Enemies')
+# Test for title
+def title_scroll():
 
-data = enemies.get_all_values()
+    title = r"""
+      ____          _    _      _                               _ 
+     / __ \        | |  | |    | |                             | |
+    | |  | |  __ _ | |_ | |__  | |__    ___   _   _  _ __    __| |
+    | |  | | / _` || __|| '_ \ | '_ \  / _ \ | | | || '_ \  / _` |
+    | |__| || (_| || |_ | | | || |_) || (_) || |_| || | | || (_| |
+     \____/  \__,_| \__||_| |_||_.__/  \___/  \__,_||_| |_| \__,_|
+                                                                """
+                                                                
+    title_lines = title.split('\n')
 
-print(data)
+    for line in title_lines:
+        print(line)
+        time.sleep(0.4)
+
+
+def main():
+    """
+    Run all functions
+    """
+    title_scroll()
+
+main()
+
+
