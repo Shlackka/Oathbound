c157f1ab7bbd495d0f940348db8b5ff5b54cda61
Oliver Morse <Olimorse_90@Hotmail.co.uk>
2024-07-30 09:51:55 +0000

    Add logic for retrieving npc list and refactor code to PEP-8 standard

--- run.py
+++ run.py
@@ -18,6 +18,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Oathbound')
 
+
 def scroll_text(text, delay=0.015):
     """
     Print text with a scrolling effect.
@@ -27,9 +28,11 @@ def scroll_text(text, delay=0.015):
         time.sleep(delay)
     print()
 
+
 def normalise_and_check_input(input_text, keywords):
     """
-    Normalise the input text and check if it contains any of the specified keywords
+    Normalise the input text
+    and check if it contains any of the specified keywords
     """
     normalised_input = input_text.strip().lower()
     for keyword in keywords:
@@ -37,20 +40,20 @@ def normalise_and_check_input(input_text, keywords):
             return True
     return False
 
+
 def title_scroll():
     """
     Main title page and introduction
     """
 
     title = r"""
-  ____          _    _      _                               _ 
+  ____          _    _      _                               _
  / __ \        | |  | |    | |                             | |
 | |  | |  __ _ | |_ | |__  | |__    ___   _   _  _ __    __| |
 | |  | | / _` || __|| '_ \ | '_ \  / _ \ | | | || '_ \  / _` |
 | |__| || (_| || |_ | | | || |_) || (_) || |_| || | | || (_| |
  \____/  \__,_| \__||_| |_||_.__/  \___/  \__,_||_| |_| \__,_|
                                                             """
-                                                                
     title_lines = title.split('\n')
 
     for line in title_lines:
@@ -58,19 +61,20 @@ def title_scroll():
         time.sleep(0.01)
 
     introduction = (
-        "Welcome to Oathbound,\n\n"
-        "In this adventure, you will embark on a journey through a fantasy world\n"
-        "filled with danger and mystery. You will choose a character class, each\n"
-        "with its own unique strengths and weaknesses. As you progress, you will\n"
-        "encounter various challenges and make decisions that will shape your\n"
-        "destiny.\n\n"
-        "Are you ready to take the oath and begin your adventure?\n\n"
-        "Press enter to proceed...\n"
+     "Welcome to Oathbound,\n\n"
+     "In this adventure, you will embark on a journey through a fantasy world\n"
+     "filled with danger and mystery. You will choose a character class, each\n"
+     "with its own unique strengths and weaknesses. As you progress, you will\n"
+     "encounter various challenges and make decisions that will shape your\n"
+     "destiny.\n\n"
+     "Are you ready to take the oath and begin your adventure?\n\n"
+     "Press enter to proceed...\n"
     )
 
     scroll_text(introduction)
 
     input('\n')
+
 
 def get_player_info():
     """
@@ -88,7 +92,7 @@ def get_player_info():
         "mage": "Mage",
         "peasant": "Peasant"
     }
-    
+
     scroll_text("Please pick a class from the following:\n")
     scroll_text("1 - Warrior")
     scroll_text("2 - Archer")
@@ -137,6 +141,7 @@ def get_player_info():
         "stats": player_stats
     }
 
+
 def initialise_game():
     turns_until_end = random.randint(10, 30)
 
@@ -146,48 +151,52 @@ def initialise_game():
 
     return turns_until_end, location
 
+
 def start_game():
     title_scroll()
+    player_info = get_player_info()
+    turns_until_end, location = initialise_game()
+    inventory = initialise_inventory(player_info)
     areas = get_areas()
     encounters = get_encounter()
     enemies = get_enemies()
+    npcs = get_npcs()
     drops = initialise_potential_drops()
-    turns_until_end, location = initialise_game()
-    player_info = get_player_info()
-    inventory = initialise_inventory(player_info)
     location_area_map = {}
     location_encounter_map = {}
-    
     game_loop(
-    player_info, 
-    location, 
-    turns_until_end, 
-    inventory, 
-    areas, 
-    location_area_map, 
-    encounters, 
-    location_encounter_map,
-    drops,
-    enemies
+        player_info,
+        location,
+        turns_until_end,
+        inventory,
+        areas,
+        location_area_map,
+        encounters,
+        location_encounter_map,
+        drops,
+        enemies,
+        npcs
     )
 
+
 def game_loop(
-    player_info, 
-    location, 
-    turns_until_end, 
-    inventory, 
-    areas, 
-    location_area_map, 
-    encounters, 
-    location_encounter_map,
-    drops,
-    enemies
-    ):
+        player_info,
+        location,
+        turns_until_end,
+        inventory,
+        areas,
+        location_area_map,
+        encounters,
+        location_encounter_map,
+        drops,
+        enemies,
+        npcs
+):
     """
     Main game loop where the player will take actions
     """
 
-    visited_locations = [(0,0)]
+    visited_locations = [(0, 0)]
 
     if location not in location_area_map:
         location_area_map[location] = get_random_area(areas)
@@ -217,7 +226,7 @@ def game_loop(
                 if new_location != (0, 0) and check_for_encounter() and new_location not in location_encounter_map:
                     encounter = get_random_encounter(encounters)
                     location_encounter_map[new_location] = encounter
-                    handle_encounter(encounter, inventory, drops, enemies, location_encounter_map, new_location)
+                    handle_encounter(encounter, inventory, drops, enemies, location_encounter_map, new_location, npcs)
                 elif new_location in location_encounter_map:
                     previous_encounter = location_encounter_map[new_location]
                     if previous_encounter == "Chest":
@@ -239,7 +248,8 @@ def game_loop(
             print("Invalid action. Please try again.")
 
     return visited_locations
-    
+
+
 def move(location, areas, location_area_map):
     """
     Function to move the player in desired direction
@@ -266,6 +276,7 @@ def move(location, areas, location_area_map):
 
     return new_location, location_area_map[new_location], True
 
+
 def get_areas():
     """
     Retrieve the list of areas from the Google Sheet
@@ -274,11 +285,13 @@ def get_areas():
     areas = area_sheet.col_values(1)
     return areas
 
+
 def get_random_area(areas):
     """
     Get a random area from the provided list of areas
     """
     return random.choice(areas)
+
 
 def get_encounter():
     """
@@ -288,11 +301,13 @@ def get_encounter():
     encounters = encounter_sheet.col_values(1)
     return encounters
 
+
 def get_random_encounter(encounters):
     """
     Get a random encounter from list of encounters
     """
     return random.choice(encounters)
+
 
 def check_for_encounter():
     """
@@ -300,9 +315,10 @@ def check_for_encounter():
     """
     return random.random() < 0.8
 
-def handle_encounter(encounter, inventory, drops, enemies, location_encounter_map, location):
+
+def handle_encounter(encounter, inventory, drops, enemies, location_encounter_map, location, npcs):
     """
-    Handle the encounter logic 
+    Handle the encounter logic
     """
     if encounter == "Chest":
         scroll_text(f"You come across a chest in front of you, \nthere is no lock on the chest and nobody around, what will you do?")
@@ -310,14 +326,14 @@ def handle_encounter(encounter, inventory, drops, enemies, location_encounter_ma
         scroll_text("2. Leave the chest alone")
 
         choice = input("")
-        
+
         open_keywords = ["open", "open the chest", "open it", "open up", "look inside", "open chest", "1"]
 
         if normalise_and_check_input(choice, open_keywords):
             if random.random() < 0.1:  # 10% chance for the chest to be a mimic
                 # Add logic to handle the fight with the mimic
-                mimic = {"Enemy": "Mimic", "Health": 50, "Attack": 12, "Speed": 4} 
-                fight_enemy(mimic)
+                mimic = {"Enemy": "Mimic", "Health": 50, "Attack": 12, "Speed": 4}
+                fight_enemy(mimic, drops, inventory)
             else:
                 drop = get_random_drop(drops)
                 scroll_text(f"You open the chest and find: {drop['Item Name']}!")
@@ -329,6 +345,11 @@ def handle_encounter(encounter, inventory, drops, enemies, location_encounter_ma
         enemy = get_random_enemy(enemies)
         fight_enemy(enemy, drops, inventory)
         location_encounter_map[location] = {"type": "Enemy", "details": enemy}
+    elif encounter == "NPC":
+        npc = get_random_npc(npcs)
+        talk_to_npc()
+        location_encounter_map[location] = {"type": "NPC", "details": npc}
+
 
 def fight_enemy(enemy, drops, inventory):
     """
@@ -340,6 +361,7 @@ def fight_enemy(enemy, drops, inventory):
     drop = get_random_drop(drops)
     scroll_text(f"You defeat the {enemy['Enemy']}, they drop a {drop['Item Name']}! You pick up the loot and continue on your journey.")
     inventory[drop['Category'] + 's'].append(drop['Item Name'])
+
 
 def initialise_inventory(player_info):
     """
@@ -365,6 +387,7 @@ def initialise_potential_drops():
     drops_data = drops_sheet.get_all_records()
     return drops_data
 
+
 def get_random_drop(drops):
     """
     Get a random drop from the list of drops
@@ -388,6 +411,22 @@ def get_random_enemy(enemies):
     return random.choice(enemies)
 
 
+def get_npcs():
+    """
+    Get list of npcs from worksheet
+    """
+    npcs_sheet = SHEET.worksheet("NPCs")
+    npcs_data = npcs_sheet.get_all_records()
+    return npcs_data
+
+
+def get_random_npc(npcs):
+    """
+    Get random NPC from list of NPCs
+    """
+    return random.choice(npcs)
+
+
 def view_inventory(inventory):
     scroll_text("Your inventory contains:")
     for category, items in inventory.items():
@@ -398,6 +437,7 @@ def view_inventory(inventory):
             for equip_category, item in items.items():
                 scroll_text(f"{equip_category}: {item if item else 'None'}")
 
+
 def view_stats(player_info):
     scroll_text(f"Player Name: {player_info['name']}")
     scroll_text(f"Player Class: {player_info['class']}")
@@ -405,12 +445,12 @@ def view_stats(player_info):
     for stat, value in player_info['stats'].items():
         scroll_text(f"{stat}: {value}")
 
+
 def main():
     """
     Run all functions
     """
     start_game()
 
+
 main()
-
-
